da5ace6e6186583197db485d7e05e36fc5c93c6f
gohelpalak <159258834+gohelpalak@users.noreply.github.com>
2025-03-12 23:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://infinitefusion.tech" }));
+app.use(cors({ origin: "https://infinitefusion.tech/" }));
 
 mongoose.connect("mongodb+srv://gohelpalak14:palak%4022@cluster0.cjwxl.mongodb.net/employees", {
   useNewUrlParser: true,
